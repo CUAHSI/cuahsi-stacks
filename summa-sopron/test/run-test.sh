9c9bd2dcce52c2a1fdddd3a41082382a3b959752
Castronova <castronova.anthony@gmail.com>
2018-10-02 13:43:13 -0400

    added summa image recipes:

---
+++ summa-sopron/test/run-test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# run the test case to make sure the container works
+docker run --rm -ti \
+-v $(pwd):/tmp/summa \
+-e LOCALBASEDIR=test01 \
+-e MASTERPATH=test01/settings/celia1990/summa_fileManager_celia1990.txt \
+cuahsi/summa:sopron -x
+
+#--entrypoint /bin/bash \
