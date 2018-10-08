c518c804f4ca28cf7cf2779e2a959db1d7cd1a13
Castronova <castronova.anthony@gmail.com>
2018-10-08 16:52:16 -0400

    importing mpish and dakota from existing images. added singleuser tests for modflow and taudem

--- singleuser/install-mpi.sh
+++ singleuser/install-mpi.sh
@@ -11,13 +11,4 @@ git submodule update --init
 ./configure --prefix=/usr
 make -j8
 make -j8 install
-rm -rf /tmp/mpich
-
-
-
-## dual install gcc, g++ 5 and 7
-#update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-7 --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-7 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-7
-#update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-5 --slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-5 --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-5
-#update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 10
-#update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
-#
+rm -rf /tmp/*
