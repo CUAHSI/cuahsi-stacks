98592905f23aa5ee8880294c4274682d6c0913b8
Castronova <castronova.anthony@gmail.com>
2020-07-15 16:10:39 -0400

    updated wrfhydro to allow different source versions

--- wrfhydro/build.sh
+++ wrfhydro/build.sh
@@ -1,3 +1,20 @@
 #!/bin/bash 
 
-docker build -t cuahsi/wrfhydro-nwm:5.0.3 .
+
+if [ $# -lt 1 ]
+then
+    echo Usage: ./build.sh [WRF_HYDRO_VERSION]
+    echo E.g. : ./build v5.1.2
+    echo
+    echo Note: version should start with 'v'. All acceptable versions can be found at https://github.com/NCAR/wrf_hydro_nwm_public/releases
+    echo
+    exit 1
+fi
+
+VERSION=$1
+TAG=$(echo $VERSION | cut -c2-)
+
+docker build \
+    --build-arg WRF_HYDRO_VERSION=$VERSION \
+    -t cuahsi/wrfhydro-nwm:$TAG \
+    .
