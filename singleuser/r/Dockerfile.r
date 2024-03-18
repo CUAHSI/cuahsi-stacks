25bf00fd589a26996bec772e657b16c87b99d213
Anthony Castronova <castronova.anthony@gmail.com>
2024-03-18 11:14:00 -0400

    adding forgotten R image changes. updating env to trigger rebuild

--- singleuser/r/Dockerfile.r
+++ singleuser/r/Dockerfile.r
@@ -1,8 +1,14 @@
 ARG BASE_VERSION
-#FROM cuahsi/singleuser-base:$BASE_VERSION
+# Singleuser-Base 2023.08.24_1
+# Ubuntu 20 - Focal
+#FROM cuahsi/singleuser-base:2023.08.24_1
+FROM cuahsi/singleuser-base:$BASE_VERSION
 
-FROM jupyter/minimal-notebook:ubuntu-20.04
-#FROM rocker/binder:4.2
+# NOTES:
+# ------------------------------------------------------------
+# The purpose of this Dockerfile is to fix the nbfetch library
+# in the R environment
+# ------------------------------------------------------------
 
 # install rstudio-server
 USER root
@@ -20,13 +26,14 @@ RUN pip install git+https://github.com/huntdatacenter/jupyter-rsession-proxy.git
 
 #  rstudio \
 
-RUN jupyter labextension install @jupyterlab/server-proxy --minimize=False
-#RUN jupyter labextension install @jupyterlab/server-proxy
+#RUN jupyter labextension install @jupyterlab/server-proxy --minimize=False
+RUN jupyter labextension install @jupyterlab/server-proxy --minimize=True
 
 USER root
-ENV RSTUDIO_VERSION=2022.07.1-554
-#1.2.5001
-RUN wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-${RSTUDIO_VERSION}-amd64.deb \
+
+# instructions: https://posit.co/download/rstudio-server/ 
+ENV RSTUDIO_VERSION=2023.09.1-494
+RUN wget https://download2.rstudio.org/server/focal/amd64/rstudio-server-${RSTUDIO_VERSION}-amd64.deb \
  && gdebi -n rstudio-server-${RSTUDIO_VERSION}-amd64.deb
 
 
@@ -53,66 +60,70 @@ RUN wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-${RSTU
 #RUN /bin/sh -c "ln -s /bin/tar /bin/gtar"
 
 
-#USER $NB_USER
-#
-## Install r-base to pinned version first to make sure the
-## correct version is installed before adding dependencies.
-#RUN mamba install -y  --quiet \
-#  --channel r \
-#  --channel conda-forge \
-#  r-base=4.2.0 \
-#&& conda clean --all -f -y \
-#&& fix-permissions $CONDA_DIR
-#
-#RUN mamba install -y --quiet \
-#  --channel r \
-#  --channel conda-forge \
-#  r-irkernel \
-#  jupyter-rsession-proxy \
-#  r-caret \
-#  r-devtools \
-#  r-forecast \
-#  r-rgdal \
-#  r-randomforest \
-#  r-ncdf4 \
-#  r-essentials \
-#  r-sf \
-#  r-sparklyr \
-#  r-tidyverse \ 
-#  r-rmarkdown \
-#  r-rjsonio \
-#  r-mapdata \
-#  r-gridextra \
-#  r-ggmap \
-#  r-rsqlite \
-#  r-hexbin \
-#  r-htmlwidgets \
-#  r-plyr \
-#&& conda clean --all -f -y \
-#&& fix-permissions $CONDA_DIR
-#
-## install non-conda packages
-#ARG PACKAGES='"WaterML","dplyer","rgrass7","dataRetrieval","stringi"'
-#RUN /opt/conda/bin/Rscript -e "install.packages(c($PACKAGES), repos='http://archive.linux.duke.edu/cran')"
-#
+USER $NB_USER
 
-#
-#USER $NB_UID
-#
-#RUN pip install jupyter-rsession-proxy
-#
-###RUN apt-get update && \
-###    curl --silent -L --fail https://download2.rstudio.org/rstudio-server-1.1.419-amd64.deb > /tmp/rstudio.deb && \
-###    echo '24cd11f0405d8372b4168fc9956e0386 /tmp/rstudio.deb' | md5sum -c - && \
-###    apt-get install -y /tmp/rstudio.deb && \
-###    rm /tmp/rstudio.deb && \
-###    apt-get clean
-##ENV PATH=$PATH:/usr/lib/rstudio-server/bin
-#
-#
-#
-#USER root
-#RUN rm -rf /home/jovan/.cache/* \
-#&& fix-permissions $CONDA_DIR \
-#&& fix-permissions /home/$NB_USER 
+# Install r-base to pinned version first to make sure the
+# correct version is installed before adding dependencies.
+RUN mamba install -y  --quiet \
+    #--channel r \
+    --channel conda-forge \
+  r-base \
+&& conda clean --all -f -y \
+&& fix-permissions $CONDA_DIR
+
+# Large Packages
+RUN mamba install -y \
+  --channel conda-forge \
+  r-sf \
+  r-caret \
+  r-tidyverse \ 
+  r-forecast \
+  r-randomforest \
+  r-essentials \
+  r-sparklyr \ 
+&& conda clean --all -f -y \
+&& fix-permissions $CONDA_DIR
+
+RUN mamba install -y \
+  --channel conda-forge \
+  r-irkernel \
+  jupyter-rsession-proxy \
+  r-ncdf4 \
+  r-rmarkdown \
+  r-rjsonio \
+  r-mapdata \
+  r-gridextra \
+  r-ggmap \
+  r-rsqlite \
+&& conda clean --all -f -y \
+&& fix-permissions $CONDA_DIR
+
+RUN mamba install -y \
+  --channel conda-forge \
+  r-dplyr \
+  r-dataretrieval \
+  r-stringi \
+  r-waterml \
+&& conda clean --all -f -y \
+&& fix-permissions $CONDA_DIR
+
+
+RUN mamba install -y \
+  --channel conda-forge \
+  r-nnls \
+  r-tictoc \
+  r-dataexplorer \
+  pyproj \
+&& conda clean --all -f -y \
+&& fix-permissions $CONDA_DIR
+
+# set projection info for use by r-sf.
+# this requires that pyproj is installed
+# on the system (see above)
+ENV PROJ_LIB='/opt/conda/share/proj/'
+
+USER root
+RUN rm -rf /home/jovan/.cache/* \
+  && fix-permissions $CONDA_DIR \
+  && fix-permissions /home/$NB_USER 
 USER $NB_UID
--- singleuser/r/build.sh
+++ singleuser/r/build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 TAG=$(echo $(hexdump -n 6 -e '4/4 "%08X" 1 "\n"' /dev/random) | tr '[:upper:]' '[:lower:]')
-TAG=0.1
+TAG=fix.r3
+RVERSION=423
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-fi
-
-docker build -f Dockerfile.r -t cuahsi/singleuser-r351:$TAG .
+docker build -f Dockerfile.r -t cuahsi/singleuser-r$RVERSION:$TAG .
 
