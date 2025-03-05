9f6f857eea3f5cfc606324a79681cc641524e84b
Anthony Castronova <castronova.anthony@gmail.com>
2025-03-05 15:34:15 -0500

    bumped rstudio-server to 2024.12.1 to match distro version

--- singleuser/r/Dockerfile.r
+++ singleuser/r/Dockerfile.r
@@ -1,14 +1,5 @@
 ARG BASE_VERSION
-# Singleuser-Base 2023.08.24_1
-# Ubuntu 20 - Focal
-#FROM cuahsi/singleuser-base:2023.08.24_1
 FROM cuahsi/singleuser-base:$BASE_VERSION
-
-# NOTES:
-# ------------------------------------------------------------
-# The purpose of this Dockerfile is to fix the nbfetch library
-# in the R environment
-# ------------------------------------------------------------
 
 # install rstudio-server
 USER root
@@ -17,61 +8,17 @@ RUN apt update \
 
 COPY logging.conf /etc/rstudio/logging.conf
 
-USER $NB_UID
-
-#RUN pip install jupyter-rsession-proxy
-RUN pip install git+https://github.com/huntdatacenter/jupyter-rsession-proxy.git@add-timeout
-
-#RUN mamba install -c conda-forge \
-
-#  rstudio \
-
-#RUN jupyter labextension install @jupyterlab/server-proxy --minimize=False
-RUN jupyter labextension install @jupyterlab/server-proxy --minimize=True
-
-USER root
-
 # instructions: https://posit.co/download/rstudio-server/ 
-ENV RSTUDIO_VERSION=2023.09.1-494
-RUN wget https://download2.rstudio.org/server/focal/amd64/rstudio-server-${RSTUDIO_VERSION}-amd64.deb \
+ENV RSTUDIO_VERSION=2024.12.1-563
+RUN wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-${RSTUDIO_VERSION}-amd64.deb \
  && gdebi -n rstudio-server-${RSTUDIO_VERSION}-amd64.deb
 
+USER $NB_UID
 
-#ENV RSERVER_TIMEOUT=30
-
-# && rm rstudio-server-2022.07.1-554-amd64.deb \
-# && apt-get clean
-
-#RUN echo "auth-revocation-list-dir=/tmp/rstudio-server-revocation-list/" >> /etc/rstudio/rserver.conf
+RUN pip install git+https://github.com/huntdatacenter/jupyter-rsession-proxy.git@add-timeout
+RUN jupyter labextension install @jupyterlab/server-proxy --minimize=True
 
 
-#RUN rm /etc/rstudio/rserver.conf
-
-#RUN apt-get update && apt-get install -y --no-install-recommends \
-#fonts-dejavu \
-#unixodbc \
-#unixodbc-dev \
-#r-cran-rodbc \
-#gfortran \
-#gcc \
-#curl \
-#&& rm -rf /var/lib/apt/lists/*
-#
-#RUN /bin/sh -c "ln -s /bin/tar /bin/gtar"
-
-
-USER $NB_USER
-
-# Install r-base to pinned version first to make sure the
-# correct version is installed before adding dependencies.
-RUN mamba install -y  --quiet \
-    #--channel r \
-    --channel conda-forge \
-  r-base \
-&& conda clean --all -f -y \
-&& fix-permissions $CONDA_DIR
-
-# Large Packages
 RUN mamba install -y \
   --channel conda-forge \
   r-sf \
