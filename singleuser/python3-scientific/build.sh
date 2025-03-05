19248486dfd351de59c8af8874d6532c900aa3d0
Anthony Castronova <castronova.anthony@gmail.com>
2025-03-05 15:02:05 -0500

    cleaned python scientific build, removed incompatible libraries

--- singleuser/python3-scientific/build.sh
+++ singleuser/python3-scientific/build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-
-docker build --build-arg BASE_VERSION=2022.06.12 -t cuahsi/singleuser:py3-sci-testing .
-
+docker build --build-arg BASE_VERSION=2025.03.03 -t cuahsi/singleuser:py3-sci-testing .
